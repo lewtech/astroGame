9141614d4d68729ed3ee65f990785643f8bed86b
lewtech <lewtech@gmail.com>
2017-05-24 15:25:22 -0500

    initial

--- game1/GameScene.swift
+++ game1/GameScene.swift
@@ -1,89 +1,115 @@
-//
-//  GameScene.swift
-//  game1
-//
-//  Created by Lew Flauta on 5/24/17.
-//  Copyright © 2017 Lew Flauta. All rights reserved.
-//
-
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    let hero = SKSpriteNode(imageNamed: "Spaceship")
+    var lastUpdateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
+    let heroMovePointsPerSec: CGFloat = 200.0
+    var velocity = CGPoint.zero
+    let playableRect: CGRect
+
+    override init(size: CGSize) {
+        let maxAspectRatio:CGFloat = 16.0/9.0 // 1
+        let playableHeight = size.width / maxAspectRatio // 2
+        let playableMargin = (size.height-playableHeight)/2.0 // 3
+        playableRect = CGRect(x: 0, y: playableMargin,
+                              width: size.width,
+                              height: playableHeight) // 4
+        super.init(size: size) // 5
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented") // 6
+    }
+
+
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        backgroundColor = SKColor.black
+        hero.position = CGPoint (x: size.width/2, y: size.height/2)
+        hero.setScale(0.33)
+        addChild(hero)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+
+
+    override func update(_ currentTime: TimeInterval){
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
         }
+        lastUpdateTime = currentTime
+        print("\(dt*1000) milliseconds since last update")
+
+        move(sprite: hero, velocity: velocity)
+        boundsCheckHero()
+
+        rotate(sprite: hero, direction: velocity)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
+    func move (sprite: SKSpriteNode, velocity: CGPoint) {
+        //1
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt),
+                                   y: velocity.y * CGFloat(dt))
+        print("Amount to move: \(amountToMove)")
+        sprite.position += amountToMove
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
+    func MoveHeroToward(location: CGPoint) {
+        let offset = location - hero.position
+        let direction = offset.normalized()
+        velocity = direction * heroMovePointsPerSec
+        //use some type of friction here
     }
-    
+
+    //MARK: TOUCH EVENTS
+    func sceneTouched (touchLocation:CGPoint){
+        MoveHeroToward(location: touchLocation)
+    }
+
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+
+        guard let touch = touches.first else {
+            return
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation:
+        touchLocation)
     }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation:
+            touchLocation)
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
+    func boundsCheckHero() {
+        let bottomLeft = CGPoint(x: 0, y: playableRect.minY)
+        let topRight = CGPoint(x: size.width, y: playableRect.maxY)
+
+        if hero.position.x <= bottomLeft.x {
+            hero.position.x = bottomLeft.x
+            velocity.x = -velocity.x
+        }
+        if hero.position.x >= topRight.x {
+            hero.position.x = topRight.x
+            velocity.x = -velocity.x
+        }
+        if hero.position.y <= bottomLeft.y {
+            hero.position.y = bottomLeft.y
+            velocity.y = -velocity.y
+        }
+        if hero.position.y >= topRight.y {
+            hero.position.y = topRight.y
+            velocity.y = -velocity.y
+        } 
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
+    func rotate(sprite: SKSpriteNode, direction: CGPoint) {
+        sprite.zRotation = direction.angle
     }
 }
